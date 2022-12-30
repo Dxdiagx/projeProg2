2d0da1a6cf0ee0a61d45278eb7b4e771dba7cc2a
dxdiag <acaarsamet@gmail.com>
2022-12-30 23:50:17 +0300

    30.12.22

--- src/main/java/com/example/odevprog/PlakaSorgu.java
+++ src/main/java/com/example/odevprog/PlakaSorgu.java
@@ -20,6 +20,7 @@ int i=0;
              printWriter.print("<html><head>");
              printWriter.print("<title>Bigiler</title>");
              printWriter.print("</head>");
+             printWriter.print(" <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">");
              printWriter.print("<body>");
              printWriter.print("<table border=2;>");
              printWriter.print("<tr>");
